41a4c2f9268e480ab725f585b6de80b929535f53
Susanmwende <mwendezmasaku@gmail.com>
2024-06-17 09:16:59 +0300

    edits

--- src/About/index.js
+++ src/About/index.js
@@ -12,7 +12,7 @@ const About = ()=>{
                 <p>
                 Loop & Wear is a cutting edge mobile app designed to connect donors with upcycling companies, promoting a more  circular lifestyle<br/>
                 and reducing waste.With its ability to browse and select from a curated list of community initiative, and the convenience of donating<br/>
-                from anywhere using mobile device Our mission is to empower individuals to make a positive impact on the environment by connecting <br/>
+                from anywhere using mobile device.Our mission is to empower individuals to make a positive impact on the environment by connecting <br/>
                 them with upcycling companies and community initiatives.Our vision is to create a world where waste is minimized and resources are <br/>
                 maximized.We envision a future where individuals, business,and communities work together to reduce waste and promote sustainability.<br/>
                 </p>
