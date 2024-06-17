92aac80cf2e5d9dbca3a2b311e97019727457a34
kesa-edna-adhiambo <edna@gmail.com>
2024-06-17 06:52:09 +0300

    adds homepage

---
+++ loopandwearhomepage/src/App.js
@@ -0,0 +1,17 @@
+import Homepage from "./Home page";
+
+const App = () => {
+return(
+  <div className = "App">
+    <p>Home</p>
+    
+  
+    
+    
+    <Homepage/>
+  </div>
+)
+}
+export default App;
+
+
